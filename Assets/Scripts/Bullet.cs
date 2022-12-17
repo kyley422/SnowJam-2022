9a163c7b2f851ec410194306c1de79bdea302632
kyley25 <kyle422@g.ucla.edu>
2022-12-17 00:02:51 -0500

    Added shooting functionality to EnemyTest scene

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,17 +5,6 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject hitEffect;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
