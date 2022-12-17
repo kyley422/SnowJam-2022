5978132c62a0f521d41bd06772d845eb64a530b5
kyley25 <kyle422@g.ucla.edu>
2022-12-16 23:22:57 -0500

    Player tracks mouse movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
+    public Camera cam;
 
     private Vector2 moveDirection;
+    private Vector2 mousePos;
 
     void Update()
     {
@@ -17,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         Move();
+        Look();
     }
 
     void ProcessInputs()
@@ -25,11 +28,20 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY);
-
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    void Look()
+    {
+        rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
