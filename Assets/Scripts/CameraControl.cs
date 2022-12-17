7abc27dcd5e6d72675467125a231a697221c3468
Channa188 <clea_h@p-hall.net>
2022-12-16 21:26:25 -0500

    0.1
    camera control script

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraZoom = 8f; // Distance between camera and 2D plane
+    public Vector2 offset = new Vector2(0f, 0f); // x-y offset of camera
+
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Constantly follow the player's transform location
+        transform.position = player.transform.position + new Vector3(offset.x, offset.y, -10);
+        Camera.main.orthographicSize = cameraZoom;
+    }
+}
