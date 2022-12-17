f87e1ac1a061fddbba3278a075db404ea459668b
kyley25 <kyle422@g.ucla.edu>
2022-12-16 23:54:34 -0500

    Added Player Shooting
    - Player fires projectiles
    - Projectiles destroyed on collision with Colliders
    - Projectiles have an effect before disappearing

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+}
