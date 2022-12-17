38ece6597f99593244250024c9aa1003148aca5c
Channa188 <clea_h@p-hall.net>
2022-12-17 15:00:41 -0500

    UI Update
    Fixed back button for credits and added all names to credits page

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -8,6 +8,7 @@ public class UIController : MonoBehaviour
 {
     public Button startButton;
     public Button creditsButton;
+    public Button backButton;
     public Label creditsText;
    
 
@@ -20,9 +21,11 @@ public class UIController : MonoBehaviour
         startButton = root.Q<Button>("StartButton");
         creditsButton = root.Q<Button>("CreditsButton");
         creditsText = root.Q<Label>("CreditsText");
+        backButton = root.Q<Button>("BackButton");
 
         startButton.clicked += StartButtonPressed;
         creditsButton.clicked += CreditsButtonPresses;
+        backButton.clicked += BackButtonPressed;
 
     }
 
@@ -33,7 +36,20 @@ public class UIController : MonoBehaviour
 
     void CreditsButtonPresses()
     {
-        creditsText.text = "Team 07";
+        creditsText.text = "Joanna Liu - 2D Art, Writtin \n\n\n" +
+                            "Catherine Sun -Programming, Audio \n\n\n" +
+                            "Laura Ness - Level Design, Programming \n\n\n" +
+                            "Clea Hannahs - Programming, UI / UX \n\n\n" +
+                            "Kyle Machnicki -Programming, Writting \n\n\n" +
+                            "My-Thuan Ha-Hoang - Level Design, Writting \n\n\n" +
+                            "Kyle Yang - Programming \n\n\n" +
+                            "Julia Lee -UI / UX, 2D Art";
+
         creditsText.style.display = DisplayStyle.Flex;
+    }
+
+    void BackButtonPressed()
+    {
+        creditsText.style.display = DisplayStyle.None;
     }
 }
