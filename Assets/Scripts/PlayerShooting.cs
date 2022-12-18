db0497ebdb0ca88383e5d4459c868af876c8317e
Channa188 <clea_h@p-hall.net>
2022-12-17 22:07:10 -0500

    Update PlayerShooting.cs
    more problems but someone else solved them!!! YAY

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -12,6 +12,7 @@ public class PlayerShooting : MonoBehaviour
     private Vector2 mousePos;
     private bool isAttacking;
     public Animator anim;
+    public Rigidbody2D rb;
 
 
     // Update is called once per frame
@@ -31,26 +32,31 @@ public class PlayerShooting : MonoBehaviour
                 {
                     isAttacking = false;
                     anim.SetBool("IsAtacking", isAttacking);
+                    StopMoving();
                 }
             }
-        }
-        
 
+        }
+
+        void Shoot()
+        {
+            Debug.Log("Shot Projectile");
+            mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 firePoint2D = new Vector2(firePoint.position.x, firePoint.position.y);
+            Vector2 aimDirection = mousePos - firePoint2D;
+
+            float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+            Quaternion radialPosition = Quaternion.Euler(0, 0, angle);
+            firePoint.rotation = radialPosition;
+
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        }
     }
 
-    void Shoot()
+    private void StopMoving()
     {
-        Debug.Log("Shot Projectile");
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 firePoint2D = new Vector2(firePoint.position.x, firePoint.position.y);
-        Vector2 aimDirection = mousePos - firePoint2D;
-
-        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
-        Quaternion radialPosition = Quaternion.Euler(0, 0, angle);
-        firePoint.rotation = radialPosition;
-
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        rb.velocity = Vector2.zero;
     }
 }
