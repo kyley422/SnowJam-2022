236a974afb3b533f7d19fd0d535f19bf580c4dbc
kyley25 <kyle422@g.ucla.edu>
2022-12-18 03:32:55 -0500

    Decreased Bullet Size

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -28,7 +28,7 @@ public class EnemyBullet : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other)
     {
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 5f);
+        Destroy(effect, .3f);
         Destroy(gameObject);      
     }
 }
--- Assets/Scripts/RadialBullet.cs
+++ Assets/Scripts/RadialBullet.cs
@@ -23,7 +23,7 @@ public class RadialBullet : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other)
     {
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 5f);
+        Destroy(effect, .3f);
         Destroy(gameObject);
     }
 }
