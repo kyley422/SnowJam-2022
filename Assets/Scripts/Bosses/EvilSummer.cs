3e7b343ae084ce9ccaa6adadce41fa11f2d45d65
kyley25 <kyle422@g.ucla.edu>
2022-12-18 04:10:38 -0500

    EvilSummer Functional!

--- Assets/Scripts/Bosses/EvilSummer.cs
+++ Assets/Scripts/Bosses/EvilSummer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class EvilSummer : MonoBehaviour
 {
@@ -23,6 +24,16 @@ public class EvilSummer : MonoBehaviour
 
     private bool isInChaseRange;
     private bool isInAttackRange;
+
+    /* Shooting variables */
+    public float attackTimer = 2f;
+    public float burstTimer = .25f;
+    public float numberOfProjectiles = 12f;
+    public float radius = 10f;
+    public float bulletSpeed = 1f;
+    public GameObject bulletPrefab;
+    private Vector2 playerPosition;
+    private float timer = 0f;
 
     public EnemyHP hp;
 
@@ -46,6 +57,14 @@ public class EvilSummer : MonoBehaviour
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         dir.Normalize();
         movement = dir;
+
+        timer += Time.deltaTime;
+        if (timer > attackTimer)
+        {
+            Burst();
+            /* Reset timer */
+            timer = timer - attackTimer;
+        }
     }
 
     private void FixedUpdate()
@@ -85,6 +104,36 @@ public class EvilSummer : MonoBehaviour
     private void MoveCharacter(Vector2 dir)
     {
         rb.MovePosition((Vector2)transform.position + (dir * speed * Time.deltaTime));
+    }
+
+    private void Burst()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    IEnumerator Shoot()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            /* Begin burst */
+            float angleStep = (float)(2 * Math.PI) / numberOfProjectiles;
+            float angle = 0f;
+
+            for (int j = 0; j < numberOfProjectiles; j++)
+            {
+                float x = transform.position.x + (float)Math.Cos(angle) * radius;
+                float y = transform.position.y + (float)Math.Sin(angle) * radius;
+
+                Vector3 bulletVector = new Vector3(x, y, 0);
+                Vector3 bulletMoveDirection = (bulletVector - transform.position).normalized * bulletSpeed;
+
+                var proj = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                proj.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletMoveDirection.x, bulletMoveDirection.y);
+
+                angle += angleStep;
+            }
+            yield return new WaitForSeconds(burstTimer);
+        }
     }
 
 }
